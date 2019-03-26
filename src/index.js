3fcc574ea0586da26a1dd47804d532f1f9403071
李见伟 <ljw5203@126.com>
2019-03-26 16:59:08 +0800

    init project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-alert('hello word..')
+class FastCache{
+    constructor(){
+        this.list = {}
+    }
+    set(key,value){
+        this.list[key] = value
+    }
+    get(key){
+        return this.list[key]
+    }
+}
+window.FastCache = FastCache
--- summary.md
+++
@@ -1,6 +0,0 @@
-# Summary
-[项目介绍](README.md)
-[使用文档](doc/use/README.md)	
-[使用1]() 
-[二次开发文档](doc/dev/README.md)
-[开发1]()
